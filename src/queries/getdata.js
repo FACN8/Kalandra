6168e07b59a1ed49a6b8d6826d55596c73c9f436
Rabea Fahoum <Rabea.fahoum@gmail.com>
2020-02-05 12:33:57 +0200

    search funtion to ivan rel #8

--- src/queries/getdata.js
+++ src/queries/getdata.js
@@ -1,10 +1,36 @@
-getEvents(cb)
+const dbConnection = require('../../db/db_connection');
 
-getComments(cb)
-getReviews(cb)
-getRegister(cb)
+const search = (term, cb) =>
+    dbConnection.query('SELECT * FROM events WHERE title LIKE $1 OR descr LIKE $2;', ['%' + term + '%', '%' + term + '%'],
+        (err, result) => {
+            if (err) return cb(err)
+            cb(null, result.rows)
+        });
+
+const getEvents = (cb) =>
+    dbConnection.query('SELECT * FROM events;',
+        (err, result) => {
+            if (err) return cb(err)
+            cb(null, result.rows)
+        });
+
+const getComments(eventId, cb) =>
+    dbConnection.query('SELECT * FROM events WHERE title LIKE $1 OR descr LIKE $2;', ['%' + term + '%', '%' + term + '%'],
+        (err, result) => {
+            if (err) return cb(err)
+            cb(null, result.rows)
+        });
+
+getReviews(eventId, cb) {
+
+}
+getRegister(eventId, cb) {
+
+}
+
 
 module.exports = {
+    search,
     getEvents,
     getComments,
     getReviews,
