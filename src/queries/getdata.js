afd294b4997496b7ef66af4f99532aee3adf9e18
Rabea Fahoum <Rabea.fahoum@gmail.com>
2020-02-05 13:04:47 +0200

    creating the get data functions closes #8

--- src/queries/getdata.js
+++ src/queries/getdata.js
@@ -10,24 +10,39 @@ const search = (term, cb) =>
 const getEvents = (cb) =>
     dbConnection.query('SELECT * FROM events;',
         (err, result) => {
-            if (err) return cb(err)
-            cb(null, result.rows)
+            if (err) return cb(err);
+            cb(null, result.rows);
         });
 
-const getComments(eventId, cb) =>
-    dbConnection.query('SELECT * FROM events WHERE title LIKE $1 OR descr LIKE $2;', ['%' + term + '%', '%' + term + '%'],
+const getComments = (eventId, cb) =>
+    dbConnection.query(
+        'SELECT comments.event_id,users.username,comments.comtext ' +
+        'FROM users join comments on comments.user_id = users.id ' +
+        'where comments.event_id = $1;', [eventId],
         (err, result) => {
             if (err) return cb(err)
             cb(null, result.rows)
         });
 
-getReviews(eventId, cb) {
+const getReviews = (eventId, cb) =>
+    dbConnection.query(
+        'SELECT reviews.event_id,users.username,reviews.revtext ' +
+        'FROM users join reviews on reviews.user_id = users.id ' +
+        'where reviews.event_id = $1;', [eventId],
+        (err, result) => {
+            if (err) return cb(err)
+            cb(null, result.rows)
+        });
 
-}
-getRegister(eventId, cb) {
 
-}
-
+const getRegister = (eventId, cb) =>
+    dbConnection.query(
+        'SELECT users.username FROM attend join users on attend.user_id ' +
+        '= users.id where attend.event_id = $1;', [eventId],
+        (err, result) => {
+            if (err) return cb(err)
+            cb(null, result.rows)
+        });
 
 module.exports = {
     search,
